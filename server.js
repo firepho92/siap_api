30ee0481857031fb4315d38ade104aacafafd7a1
Alexandro Aguilar <auza920205@gmail.com>
2017-08-29 10:02:55 -0500

    Server port change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3030,
+    port = process.env.PORT || 80,
     mongoose = require('mongoose'),
     Pregunta = require('./api/models/preguntasModel'),
     Tipo = require('./api/models/tiposModel'),
@@ -11,30 +11,30 @@ var express = require('express'),
 
     bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Test');
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/Test');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
+    app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+      // Pass to next layer of middleware
+      next();
+  });
 
 var preguntasRoutes = require('./api/routes/preguntasRoutes'),
     tiposRoutes = require('./api/routes/tiposRoutes'),
